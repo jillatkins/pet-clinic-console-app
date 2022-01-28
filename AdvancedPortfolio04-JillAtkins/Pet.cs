d581c4ead9b4212d50960c8020b6b078747e61e3
Jill Atkins <jillatkins.dev@gmail.com>
2022-01-27 23:29:34 -0700

    Pet Clinic Console App Upload Init

---
+++ AdvancedPortfolio04-JillAtkins/Pet.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedPortfolio04_JillAtkins
+{
+    class Pet
+    {
+        // Instance fields
+        private string Name = "Spot";
+        private int Age = 1;
+        private double Weight = 5;
+        private char Type = 'D';
+
+        // Methods
+
+        // No-argument constructor (default)
+        public Pet()
+        {
+
+        } // end default constructor
+
+        public Pet(string name, int age, double weight, char type)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Weight = weight;
+            this.Type = type;
+        } // end constructor method
+
+        public string GetPetName()
+        {
+            // Returns the pet's name
+            return this.Name;
+        } // end GetPetName() method
+
+        public string SetPetName()
+        {
+            // This method changes the pet's name
+            bool validInput = false;
+            string newPetName;
+
+            do
+            {
+                Console.Write("Enter the name of your pet: ");
+                newPetName = Program.ChangeInputTextToRed();
+                
+                // Check if entered pet name is empty
+                if (String.IsNullOrEmpty(newPetName) || newPetName == " ")
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Invalid input value. A pet name is required and " +
+                                      "must contain at least one character.");
+                    Console.ResetColor();
+                } // end if
+
+                else
+                {
+                    validInput = true;
+                    this.Name = newPetName;
+                } // end else
+
+            } while (validInput == false);
+
+            return this.Name;
+            
+        } // end SetPetName() mutator method
+
+        public int GetPetAge()
+        {
+            // Returns the pet's age
+            return this.Age;
+        } // end GetPetAge() method
+
+        public int SetPetAge()
+        {
+            // This method changes the pet's age
+            
+            string userInput;
+            bool validInput = false;
+            int petAge;
+
+            do
+            {
+                Console.Write("Enter the age of your pet: ");
+                userInput = Program.ChangeInputTextToRed();
+
+                try
+                {
+                    petAge = int.Parse(userInput);
+
+                    if (petAge > 0)
+                    {
+                        validInput = true;
+                        this.Age = petAge;
+                    } // end if
+
+                    else
+                    {
+                        validInput = false;
+                        throw new Exception();
+                    } // end else
+
+                } // end try
+
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Invalid input value. Age must be at least 1 year old.");
+                    Console.ResetColor();
+                } // end catch
+
+            } while (validInput == false);
+
+            return this.Age;
+        } // end SetPetAge() method
+
+       public double GetPetWeight()
+        {
+            // Returns the pet's weight
+            return this.Weight;
+        } // end GetPetWeight() method
+
+        public double SetPetWeight()
+        {
+            // This method changes the pet's weight
+            
+            bool validInput = false;
+            double petWeight;
+
+            // Check if pet weight is equal to or greater than 5
+            do
+            {
+                Console.Write("Enter the weight (in pounds) of your pet: ");
+
+                petWeight = Program.GetUserDouble();
+
+                if (petWeight >= 5)
+                {
+                    this.Weight = petWeight;
+                    validInput = true;
+                } // end if
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Invalid input value. Weight must be at least 5 pounds.");
+                    Console.ResetColor();
+                } // end else
+
+            } while (validInput == false);
+
+            return this.Weight;
+        } // end SetPetWeight() mutator method
+
+        public char GetPetType()
+        {
+            // This method returns the pet's type
+            return this.Type;
+        } // end GetPetType() method
+
+        public char SetPetType()
+        {
+            // This method changes the pet's type
+            
+            bool validInput = false;
+            char petType;
+
+            // Prompt & Check if pet type is equal to D or C
+            do
+            {
+                Console.Write("Enter D for Dog, C for cat: ");
+
+                petType = Program.GetUserChar();
+
+                if (petType == 'D' || petType == 'C')
+                {
+                    this.Type = petType;
+                    validInput = true;
+                } // end if
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Invalid input value. Pet type must be D or C.");
+                    Console.ResetColor();
+                } // end else
+
+            } while (validInput == false);
+
+            return this.Type;
+        } // end SetPetType() mutator method
+
+        public double Acepromazine()
+        {
+           // This method calculates the dosage for Acepromazine
+            
+            double acepromazineDosage = 0;
+            double petWeightKG;
+
+            // Convert petweight from LBS to KG
+            petWeightKG = this.Weight / 2.205;
+
+            if (this.Type == 'D')
+            {
+                acepromazineDosage = petWeightKG * (0.03/10);
+            } // end if
+
+            else if (this.Type == 'C')
+            {
+                acepromazineDosage = petWeightKG * (0.002 / 10);
+            } // end else-if
+
+            acepromazineDosage = Math.Round(acepromazineDosage, 3);
+
+            return acepromazineDosage;
+        } // end Acepromazine() method
+
+        public double Carprofen()
+        {
+            // This method calculates the dosage for Carprofen
+
+            double carprofenDosage = 0;
+            double petWeightKG;
+
+            // Convert petweight from LBS to KG
+            petWeightKG = this.Weight / 2.205;
+
+            if (this.Type == 'D')
+            {
+                carprofenDosage = petWeightKG * (0.5 / 12);
+            } // end if
+
+            else if (this.Type == 'C')
+            {
+                carprofenDosage = petWeightKG * (0.25 / 12);
+            } // end else-if
+
+            carprofenDosage = Math.Round(carprofenDosage, 3);
+
+            return carprofenDosage;
+        } // end Acepromazine() method
+
+    } // end class
+} // end namespace
---
+++ AdvancedPortfolio04-JillAtkins/Program.cs
@@ -0,0 +1,410 @@
+﻿/*
+Purpose: Advanced Portfolio #4 - Classes and Objects (Pet Clinic)
+
+Input: - Pet name
+       - Pet age
+       - Pet weight
+       - Pet type (dog or cat)
+       - Confirmation of info
+       - Service type
+       - Other pet needing service (y or n)
+
+Processes: - Prompt user for input
+           - Validate user input
+           - Calculate medication doses
+           - Write info to CSV file
+
+Output: - Prompts
+        - Pet information
+        - Medication information
+ 
+Author: Jill Atkins
+Last Modified: 2020.04.21
+*/
+
+using System;
+using System.IO;
+
+namespace AdvancedPortfolio04_JillAtkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DECLARE
+            string petName;
+            int petAge, serviceType;
+            double petWeight;
+            char petType;
+            bool additionalPet = true;
+
+            // Create the StreamWriter/file, add on to the existing data (do not overwrite it)
+            StreamWriter petMedication = new StreamWriter("../../../../pet-medication.csv", true);
+
+            WelcomeMessage();
+
+            do
+            {
+                bool correctInfo = false;
+
+                // Create instance
+                Pet myPet = new Pet();
+
+                do
+                {
+                    // Get pet info from user
+                    UserPrompts(myPet);
+
+                    // Display entered pet info, confirm correctness
+                    correctInfo = CorrectPetInfo(petName = myPet.GetPetName(), 
+                                 petAge = myPet.GetPetAge(), petWeight = myPet.GetPetWeight(), 
+                                 petType = myPet.GetPetType());
+
+                } while (correctInfo == false);
+
+                // Display the service options, get service type from user
+                serviceType = ServiceOptions();
+
+                // calculate the pet's medication & write info to file
+                CalculateMedications(myPet, serviceType, petName, petAge, petWeight, petType, petMedication);
+
+                // Ask user if additional pet needs service
+                additionalPet = AdditionalPet();
+
+            } while (additionalPet == true);
+
+            petMedication.Close(); // Close the stream writer
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\nGood-bye and thanks for coming to the Pet Clinic.\n");
+            Console.ResetColor();
+
+        } // end Main() method
+
+        static void WelcomeMessage()
+        {
+            // Program's startup message
+            Console.WriteLine("|---------------------------------------|");
+            Console.WriteLine("| CPSC1012 Pet Clinic                   |");
+            Console.WriteLine("|---------------------------------------|");
+        } // end WelcomeMessage() method
+
+        static void UserPrompts(Pet myPet)
+        {
+            // This method gets the pet's information from the user and saves it in the Pet class
+            
+            string petName;
+            int petAge;
+            double petWeight;
+            char petType;
+            
+            // Prompt for pet name
+            petName = myPet.SetPetName();
+
+            // Prompt for pet age
+            petAge = myPet.SetPetAge();
+
+            // Prompt for pet weight
+            petWeight = myPet.SetPetWeight();
+
+            // Prompt for pet type
+            petType = myPet.SetPetType();
+
+        } // end UserPrompts() method
+
+        public static int GetUserInt()
+        {
+            // This method gets a valid integer from the user
+            string userInput;
+            int userInt = 0;
+            bool valid = false;
+
+            do
+            {
+                userInput = ChangeInputTextToRed();
+                try
+                {
+                    userInt = int.Parse(userInput);
+
+                    if (userInt > 0)
+                    {
+                        valid = true;
+                    } // end if
+
+                    else
+                    {
+                        valid = false;
+                        throw new Exception();
+                    } // end else
+
+                } // end try
+
+                catch
+                {
+                    Console.WriteLine("Invalid input. Please enter a positive integer.");
+                } // end catch
+
+            } while (valid == false);
+
+            return userInt;
+        } // end GetUserInt() method
+
+        public static double GetUserDouble()
+        {
+            // This method gets a valid double from the user
+            string userInput;
+            double userDouble = 0;
+
+            userInput = ChangeInputTextToRed();
+            try
+            {
+                userDouble = double.Parse(userInput);
+            } // end try
+
+            catch
+            {
+                // The error message is the method myPet.SetPetWeight()
+
+            } // end catch
+
+            return userDouble;
+        } // end GetUserDouble() method
+
+        public static char GetUserChar()
+        {
+            // This method gets a valid char (letter) input from the user
+
+            // DECLARE
+            string userInput;
+            char userChar = ' ';
+            bool validInput = false, validLetter = false;
+
+            // Prompt the user for input until valid input is entered
+            do
+            {
+                userInput = ChangeInputTextToRed();
+                try
+                {
+                    userChar = char.Parse(userInput);
+                    userChar = char.ToUpper(userChar); // force the letter to uppercase
+
+                    // check if the letter entered is a Letter Char (not a number or other character)
+                    validLetter = Char.IsLetter(userChar);
+
+                    if (validLetter == true)
+                    {
+                        validInput = true;
+                    } // end if
+
+                    else
+                    {
+                        throw new Exception();
+                    } // end else
+
+                } // end try
+
+                catch
+                {
+                    Console.WriteLine("Invalid input. Please enter a single letter.");
+                } // end catch
+
+            } while (validInput == false);
+
+            return userChar;
+        } // end GetUserChar() method
+
+        static bool CorrectPetInfo(string petName, int petAge, double petWeight, char petType)
+        {
+            // This method displays the inputted pet info, and gets confirmation from the user
+            
+            bool correctInfo = false, validInput = false;
+            char validInfo;
+            string pet;
+
+            // Convert the pet type to a string
+            if (petType == 'C')
+            {
+                pet = "Cat";
+            }
+
+            else
+            {
+                pet = "Dog";
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Name: {petName}, Age: {petAge} years, Weight: {petWeight} lb, Type: {pet}");
+            Console.ResetColor();
+            do
+            {
+                Console.Write("Is the information above about your pet correct? Enter y or n: ");
+
+                validInfo = GetUserChar();
+
+                // If yes, continue with program
+                if (validInfo == 'Y')
+                {
+                    validInput = true;
+                    correctInfo = true;
+                } // end if
+
+                // If no, re-prompt the user for pet info
+                else if (validInfo == 'N')
+                {
+                    validInput = true;
+                    correctInfo = false;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input value. Enter Y or N.");
+                } // end else
+
+            } while (validInput == false);
+
+            return correctInfo;
+        }
+
+        static int ServiceOptions()
+        {
+            // This method displays the Service Option menu and
+            // returns the service option value inputted by the user
+            
+            int serviceOption;
+
+            Console.WriteLine("\nService Options");
+            Console.WriteLine("1.\tPain Killer");
+            Console.WriteLine("2.\tSedative");
+            Console.WriteLine("3.\tBoth Pain Killer and Sedative");
+            Console.Write("Enter the service (1-3) required for your pet: ");
+
+            serviceOption = GetUserInt();
+
+            return serviceOption;
+
+        } // end ServiceOptions() method
+
+        static bool AdditionalPet()
+        {
+            // This method asks the user if they have another pet that requires service
+            
+            bool additionalPet = false, validInput = false;
+            char userInput;
+
+            do
+            {
+                Console.Write("\nDo you have another pet that requires service? Enter y or n: ");
+
+                userInput = GetUserChar();
+
+                // If Yes, re-run the prompts
+                if (userInput == 'Y')
+                {
+                    validInput = true;
+                    additionalPet = true;
+                } // end if
+
+                // If no, end the program
+                else if (userInput == 'N')
+                {
+                    validInput = true;
+                    additionalPet = false;
+                } // end else-if
+
+                else
+                {
+                    Console.WriteLine("Invalid input value. Enter Y or N.");
+                } // end else
+
+            } while (validInput == false);
+
+            return additionalPet;
+        } // end AdditionalPet() method
+
+        static void WriteToFile(StreamWriter petMedication, string petName, double carprofenDose, double acepromazineDose)
+        {
+            // This method writes the pet's information to a CSV file
+            
+            string medication = " ";
+            DateTime currentDateTime = DateTime.Now;
+
+            // If the pet requires carprofen
+            if (carprofenDose > 0 && acepromazineDose == 0)
+            {
+                medication = "carprofen";
+                petMedication.WriteLine(currentDateTime.ToString("MM/dd/yyyy hh:mm tt") + "," + petName + "," +
+                medication + "," + carprofenDose + "ml");
+            } // end if
+
+            // If the pet requires acepromazine
+            else if (carprofenDose == 0 && acepromazineDose > 0)
+            {
+                medication = "acepromazine";
+                petMedication.WriteLine(currentDateTime.ToString("MM/dd/yyyy hh:mm tt") + "," + petName + "," +
+                medication + "," + acepromazineDose + "ml");
+            } // end else-if
+
+            // If the pet requires BOTH medications
+            else if (carprofenDose > 0 && acepromazineDose > 0)
+            {
+                petMedication.WriteLine(currentDateTime.ToString("MM/dd/yyyy hh:mm tt") + "," + petName + "," +
+                "carprofen" + "," + carprofenDose + "ml");
+
+                petMedication.WriteLine(currentDateTime.ToString("MM/dd/yyyy hh:mm tt") + "," + petName + "," +
+                "acepromazine" + "," + acepromazineDose + "ml");
+            } // end else-if
+            
+        } // end WriteToFile method
+
+        static void CalculateMedications(Pet myPet, int serviceType, string petName, int petAge, double petWeight, char petType, StreamWriter petMedication)
+        {
+            // This method calculates the medication dosages for each type of medication
+            
+            double acepromazineDose = 0, carprofenDose = 0;
+            Console.WriteLine(); // whitespace
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            // If the pet requires carprofen
+            if (serviceType == 1)
+            {
+                carprofenDose = myPet.Carprofen();
+                Console.WriteLine($"Your pet requires {carprofenDose}ml of Carprofen.");
+            } // end if
+
+            // If the pet requires acepromazine
+            else if (serviceType == 2)
+            {
+                acepromazineDose = myPet.Acepromazine();
+                Console.WriteLine($"Your pet requires {acepromazineDose}ml of Acepromazine.");
+            } // end else-if
+
+            // If the pet requires BOTH medications
+            else if (serviceType == 3)
+            {
+                carprofenDose = myPet.Carprofen();
+                acepromazineDose = myPet.Acepromazine();
+
+                Console.WriteLine($"Your pet requires {carprofenDose}ml of Carprofen.");
+                Console.WriteLine($"Your pet requires {acepromazineDose}ml of Acepromazine.");
+            } // end else-if
+            Console.ResetColor();
+
+            // Write this info to the file
+            WriteToFile(petMedication, petName, carprofenDose, acepromazineDose);
+
+        } // end CalculateMedications() method
+
+        public static string ChangeInputTextToRed()
+        {
+            // This method changes all user input text to red
+            string userInput;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            userInput = Console.ReadLine();
+            Console.ResetColor();
+
+            return userInput;
+        } // end ChangeInputTextToRed() method
+
+    } // end class
+} // end namespace
